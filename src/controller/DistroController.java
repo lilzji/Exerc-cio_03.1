059f0336141658aebc65aee4b63c3891bc23e657
Leonardo Ferreira <74208182+lilzji@users.noreply.github.com>
2024-03-02 18:25:31 -0300

    Primeiro Commit

---
+++ src/controller/DistroController.java
@@ -0,0 +1,42 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class DistroController {
+	public DistroController() {
+		super();
+	}
+
+	private String os() {
+		String os = System.getProperty("os.name");
+		return os;
+	}
+
+	public void exibeDistro() {
+		String os = os();
+		if (os.contains("Linux")) {
+			String linux = "cat /etc/os-release";
+			try {
+				Process pLinux = Runtime.getRuntime().exec(linux);
+				InputStream fluxoLinux = pLinux.getInputStream();
+				InputStreamReader leitorLinux = new InputStreamReader(fluxoLinux);
+				BufferedReader bufferLinux = new BufferedReader(leitorLinux);
+				String linhaLinux = bufferLinux.readLine();
+				while (linhaLinux != null) {
+					System.out.println(linhaLinux);
+					linhaLinux = bufferLinux.readLine();
+				}
+				bufferLinux.close();
+				leitorLinux.close();
+				fluxoLinux.close();
+			} catch (Exception e) {
+
+			}
+		} else {
+			System.err.println("O SISTEMA OPERACIONAL NAO EH LINUX");
+		}
+	}
+}
